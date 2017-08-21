1db075fa3dc14bfec89e795b5c5e6a5655a60492
120805087 <120805087@qq.com>
2017-08-21 18:22:28 +0800

    first music

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+    <div id="app">
+        <m-header></m-header>
+        <tab></tab>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+import MHeader from "components/m-header/m-header"
+import Tab from "components/tab/tab"
+export default {
+    components: {
+        MHeader,
+        Tab
+    }
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>
---
+++ src/components/search/search.vue
@@ -0,0 +1,13 @@
+<template lang="html">
+    <div class="search">
+        wo shi serach
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
